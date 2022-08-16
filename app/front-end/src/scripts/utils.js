54e90873f00ee46c443647bdded5144f155330d4
Ankit Kumar <ankit.kr554@gmail.com>
2022-08-16 10:06:31 +0530

    contract integration

--- app/front-end/src/scripts/utils.js
+++ app/front-end/src/scripts/utils.js
@@ -1,81 +1,198 @@
-import { ethers } from 'ethers';
+import { ethers } from "ethers";
 
-const Web3 = require('web3')
+const Web3 = require("web3");
 // require('dotenv').config()
-const web3 = new Web3("https://goerli.infura.io/v3/900b48c2d82a44d09ba0d57901aa662a")
+const web3 = new Web3(
+  "https://goerli.infura.io/v3/900b48c2d82a44d09ba0d57901aa662a"
+);
 
 let USDCtokenAddress = process.env.USDCtokenAddress;
-let INRtokenAddress = process.env.INRtokenAddress;
+// let INRtokenAddress = process.env.INRtokenAddress;
+let INRtokenAddress = "0xEAd7e4b5553BB19F42d249A30AEea8Ea1D633ea9";
 let USDC_ABI = JSON.parse(process.env.USDC_ABI);
-let INR_ABI = JSON.parse(process.env.INR_ABI);
+// let INR_ABI = JSON.parse(process.env.INR_ABI);
+let INR_ABI = JSON.parse('[{"inputs":[{"internalType":"uint256","name":"initialSupply","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"id","type":"bytes32"}],"name":"ChainlinkCancelled","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"id","type":"bytes32"}],"name":"ChainlinkFulfilled","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"id","type":"bytes32"}],"name":"ChainlinkRequested","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"}],"name":"OwnershipTransferRequested","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"requestId","type":"bytes32"},{"indexed":false,"internalType":"uint256","name":"volume","type":"uint256"}],"name":"RequestVolume","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[],"name":"acceptOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"burn","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_requestId","type":"bytes32"},{"internalType":"uint256","name":"_rate","type":"uint256"}],"name":"fulfill","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"mint","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"presenter","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"rate","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"requestVolumeData","outputs":[{"internalType":"bytes32","name":"requestId","type":"bytes32"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdrawLink","outputs":[],"stateMutability":"nonpayable","type":"function"}]');
 // The minimum ABI to get ERC20 Token balance
 let minABI = [
   // balanceOf
   {
-    "constant":true,
-    "inputs":[{"name":"_owner","type":"address"}],
-    "name":"balanceOf",
-    "outputs":[{"name":"balance","type":"uint256"}],
-    "type":"function"
+    constant: true,
+    inputs: [{ name: "_owner", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ name: "balance", type: "uint256" }],
+    type: "function",
   },
   // decimals
   {
-    "constant":true,
-    "inputs":[],
-    "name":"decimals",
-    "outputs":[{"name":"","type":"uint8"}],
-    "type":"function"
+    constant: true,
+    inputs: [],
+    name: "decimals",
+    outputs: [{ name: "", type: "uint8" }],
+    type: "function",
   },
-   // transfer
- {
-  "constant": false,
-  "inputs": [
-   {
-    "name": "_to",
-    "type": "address"
-   },
-   {
-    "name": "_value",
-    "type": "uint256"
-   }
-  ],
-  "name": "transfer",
-  "outputs": [
-   {
-    "name": "",
-    "type": "bool"
-   }
-  ],
-  "type": "function"
- }
+  // transfer
+  {
+    constant: false,
+    inputs: [
+      {
+        name: "_to",
+        type: "address",
+      },
+      {
+        name: "_value",
+        type: "uint256",
+      },
+    ],
+    name: "transfer",
+    outputs: [
+      {
+        name: "",
+        type: "bool",
+      },
+    ],
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: "_spender",
+        type: "address",
+      },
+      {
+        name: "_value",
+        type: "uint256",
+      },
+    ],
+    name: "approve",
+    outputs: [
+      {
+        name: "",
+        type: "bool",
+      },
+    ],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: "_from",
+        type: "address",
+      },
+      {
+        name: "_to",
+        type: "address",
+      },
+      {
+        name: "_value",
+        type: "uint256",
+      },
+    ],
+    name: "transferFrom",
+    outputs: [
+      {
+        name: "",
+        type: "bool",
+      },
+    ],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+	{
+		inputs: [
+			{
+				internalType: "uint256",
+				name: "amount",
+				type: "uint256"
+			}
+		],
+		name: "mint",
+		outputs: [],
+		stateMutability: "nonpayable",
+		type: "function"
+	},
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256"
+      }
+    ],
+    name: "burn",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function"
+  },
 ];
 
-
 export async function getBalances(walletAddress) {
-    let USDCcontract = new web3.eth.Contract(minABI,USDCtokenAddress);
-    let INRCcontract = new web3.eth.Contract(minABI,INRtokenAddress);
-    let balanceUSDC = await USDCcontract.methods.balanceOf(walletAddress).call();
-    let balanceINR =  await INRCcontract.methods.balanceOf(walletAddress).call();
-    return JSON.stringify({"USDC": balanceUSDC, "INR": balanceINR});
+  let USDCcontract = new web3.eth.Contract(minABI, USDCtokenAddress);
+  let INRCcontract = new web3.eth.Contract(INR_ABI, INRtokenAddress);
+  let balanceUSDC = await USDCcontract.methods.balanceOf(walletAddress).call();
+  let balanceINR = await INRCcontract.methods.balanceOf(walletAddress).call();
+  return JSON.stringify({ USDC: balanceUSDC, INR: balanceINR });
 }
 
 export async function mintINR(amount) {
-  console.log("USDC", USDC_ABI)
-  console.log("INR_ABI",INR_ABI)
-  if (typeof window.ethereum !== 'undefined') {
+  console.log("USDC", USDC_ABI);
+  console.log("INR_ABI", INR_ABI);
+  if (typeof window.ethereum !== "undefined") {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner()
-    const contract = new ethers.Contract(USDCtokenAddress, minABI, signer)
-    contract.transfer(INRtokenAddress, amount).then(data => console.log({ data }))
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(USDCtokenAddress, minABI, signer);
+    const INRContract = new ethers.Contract(INRtokenAddress,minABI,signer);
+    contract.approve(INRtokenAddress, amount).then(
+      (x) => console.log(x),
+      (x) => console.log(x),
+      INRContract.mint(amount).then(data => console.log({ data }))
+
+    );
+    // contract.mint(amount).then(data => console.log({ data }));
+    // INRContract.mint(amount).then(data => console.log({ data }))
+    // contract.transferFrom(INRtokenAddress, amount).then(data => console.log({ data }))
   }
 }
 export async function burnINR(amount) {
-  console.log("USDC", USDC_ABI)
-  console.log("INR_ABI",INR_ABI)
-  if (typeof window.ethereum !== 'undefined') {
+  console.log("USDC", USDC_ABI);
+  console.log("INR_ABI", INR_ABI);
+  if (typeof window.ethereum !== "undefined") {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner()
-    const contract = new ethers.Contract(INRtokenAddress, minABI, signer)
-    contract.transfer(INRtokenAddress, amount).then(data => console.log({ data }))
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(INRtokenAddress, minABI, signer);
+    contract.burn(amount).then(data => console.log({ data }))
+    // contract
+    //   .transfer(INRtokenAddress, amount)
+    //   .then((data) => console.log({ data }));
   }
 }
+
+
+// const derc20contract = '0x36850b80ad73fe66216609B9796ed6ceae8BE29F';
+// export const handleClick = async (e) => {
+//             e.preventDefault();
+//             const prtmp = await new ethers.providers.Web3Provider(window.ethereum);
+            
+//             // -------------------- Approve Part ------------------------------// 
+//             const web3 = new Web3(prtmp);
+//             const erc20contract = new web3.eth.Contract(
+//                 erc20abi, 
+//                 derc20token,
+//                 { from: '0xFromAddress' }
+//             );
+
+//             await erc20contract.methods.approve(derc20contract, web3.utils.toHex(3e18)).send();
+//             // ---------------------------------------------------------------//
+
+//             const contract = new web3.eth.Contract(derc20abi, derc20contract);
+//             const response = await contract.methods.send_usdt(
+//                 '0xToAddress',
+//                 web3.utils.toHex(3e18)
+//             )
+//             .call({ from: '0xFromAddress'});
+//             console.log(response);
+//     };
